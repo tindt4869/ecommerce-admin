133326202d7b46939d8a1e8042ad4a7f3c7d1778
tindt <18420952+tindt4869@users.noreply.github.com>
2023-11-27 22:28:37 +0700

    feat: finish sizes, colors

---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ColorsClient } from "./components/client";
+import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
+
+async function ColorsPage({ params }: { params: { storeId: string } }) {
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedColors = colors.map((item) => {
+    return {
+      id: item.id,
+      name: item.name,
+      value: item.value,
+      createdAt: format(item.createdAt, "MMMM do, yyyy"),
+    };
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorsClient data={formattedColors} />
+      </div>
+    </div>
+  );
+}
+
+export default ColorsPage;
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -26,6 +26,16 @@ export function MainNav({ className, ...props }: HTMLAttributes<HTMLElement>) {
       active: pathname === `/${params.storeId}/categories`,
     },
     {
+      href: `/${params.storeId}/sizes`,
+      label: "Sizes",
+      active: pathname === `/${params.storeId}/sizes`,
+    },
+    {
+      href: `/${params.storeId}/colors`,
+      label: "Colors",
+      active: pathname === `/${params.storeId}/colors`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
